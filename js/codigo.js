1e2cc22e7c6ce95e453f1ceef24bedcfaca13464
mcaruisan <mcarmen.rs79@gmail.com>
2024-11-04 17:11:13 +0100

    creación mini proyecto

---
+++ js/codigo.js
@@ -0,0 +1,28 @@
+
+(() => {
+    'use strict';
+    const forms = document.querySelectorAll('.needs-validation');
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault();
+                event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+        }, false);
+    });
+})();
+
+// Script de validación del login 
+
+(function() {
+    'use strict';
+    const form = document.getElementById('loginForm');
+    form.addEventListener('submit', function(event) {
+        if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+    }, false);
+})();
